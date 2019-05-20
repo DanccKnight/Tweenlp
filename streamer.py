6e9e4b025552a4622279908710951e1fd129a51d
Ansuman Acharya <nitinansu12@gmail.com>
2019-05-20 19:29:32 +0530

    Made things modular

--- streamer.py
+++ streamer.py
@@ -5,19 +5,35 @@ import credentials
 
 class Listener(StreamListener):
 	def on_data(self,data):
-		print(data)
-		return True
+		try:
+			print(data)
+			with open(self.fetched_tweets,'a') as tf:
+				tf.write(data)
+			return True
+		except BaseException as e:
+			print(str(e))
+		return True 
 
-	def on_error(self,status):
+	def on_error(self, status):
 		print(status)
 		return False 
+	
+	def __init__(self, fetched_tweets):
+		self.fetched_tweets = fetched_tweets
+	
+class TStreamer():
+	
+	def stream_tweets(self, fetched_tweets, hashtag):
+		listener = Listener(fetched_tweets)
+		auth = OAuthHandler(credentials.CONSUMER_KEY,credentials.CONSUMER_SECRET)
+		auth.set_access_token(credentials.ACCESS_TOKEN,credentials.ACCESS_TOKEN_SECRET)
+		
+		stream = Stream(auth,listener)
+		stream.filter(track=hashtag)
 
 if __name__ == "__main__":
-	listener = Listener()
-	auth = OAuthHandler(credentials.CONSUMER_KEY,credentials.CONSUMER_SECRET)
-	auth.set_access_token(credentials.ACCESS_TOKEN,credentials.ACCESS_TOKEN_SECRET)
 	
-	stream = Stream(auth,listener)
-	
-	x=input("Enter the keyword you want to track:")
-	stream.filter(track=[x])
+	hashtag = ["Pewdiepie"]
+	fetched_tweets = "tweets.json"
+	twitter_Streamer = TStreamer()
+	twitter_Streamer.stream_tweets(fetched_tweets, hashtag)
