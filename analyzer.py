07d04d6d5a4a06be79e3a711c7580eb19e5ef07a
Ansuman Acharya <nitinansu12@gmail.com>
2019-05-24 23:21:53 +0530

    It works, I think

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,16 @@ import numpy as np
 
 path = '/home/ansuman/Twitter-NLP/tweets.txt'
 
+def isReply(tweet):
+	if(tweet['in_reply_to_status_id']): 
+	#or tweet['in_reply_to_status_id_str'] 
+	#or tweet['in_reply_to_user_id'] 
+	#or tweet['in_reply_to_user_id_str']
+	#or tweet['in_reply_to_screen_name']):
+		return 1
+	else: 
+		return 0
+
 tweets_data = []
 tweets_text = []
 tweets_file = open(path,"r")
@@ -15,11 +25,12 @@ for line in tweets_file:
 		print(str(e))
 
 for i in range(len(tweets_data)):
-	tweets_text.append(tweets_data[i]['text'])
+		if(isReply(tweets_data[i])):
+			tweets_text.append(tweets_data[i]['text'])
 
 for i in range(len(tweets_text)):
 	print(tweets_text[i])
 
-#tweets = pd.DataFrame(data=[tweets_data['text'] for tweet in tweets_data],columns=['Tweets'])
+#tweets = pd.DataFrame(data=[tweets_text['text'] for tweet in tweets_text],columns=['Tweets'])
 #tweets['Retweet_Count'] = np.array([tweets_data['retweet_count'] for tweet in tweets_data])
 #print(pd)
