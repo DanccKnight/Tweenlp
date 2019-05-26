52c6856b32bef320ac6958d019f32f5e84febd33
Ansuman Acharya <nitinansu12@gmail.com>
2019-05-26 22:36:53 +0530

    Sentiment Analysis added

--- usertweets.py
+++ usertweets.py
@@ -4,7 +4,8 @@ from tweepy import API
 from tweepy import OAuthHandler
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from textblob import TextBlob
+import re
 
 class Authenticator():
 	def authenticate(self):
@@ -16,17 +17,10 @@ class TwitterClient():
 	def __init__(self, user=None):
 		self.auth = Authenticator().authenticate()
 		self.twitter_client = API(self.auth)
-#		self.user = user
 
 	def get_twitter_client_api(self):
 		return self.twitter_client
 	
-#	def get_user_tweets(self, num_tweets, user_fetched_tweets):
-#		for tweet in Cursor(self.twitter_client.user_timeline, id = self.user).items(num_tweets):
-#			print(tweet)
-#			with open(user_fetched_tweets,'a') as file:
-#				file.write(str(tweet))
-
 class Analyzer():
 	def tweet_to_dataframe(self,tweets):
 		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
@@ -34,23 +28,22 @@ class Analyzer():
 		df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 		df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
 		return df
+	
+	def clean_tweet_text(self,tweet):
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+	def get_sentiment(self,tweet):
+		analysis = TextBlob(self.clean_tweet_text(tweet))
+		#print(analysis)
+		return analysis.sentiment.polarity		
 
 if __name__ == "__main__":
 	tweet_analyzer = Analyzer()
 	twitter_client = TwitterClient()
 
-#	user_fetched_tweets = "userfetchedtweets.json"
-#	print(twitter_client.get_user_tweets(4,user_fetched_tweets))
 	api = twitter_client.get_twitter_client_api()
 	tweets = api.user_timeline(screen_name="pewdiepie",count=20)
 	df = tweet_analyzer.tweet_to_dataframe(tweets)
-	#print(df)
-	print("Average no. of retweets:",np.mean(df['retweets']))
-	print("Highest liked tweet:",np.max(df['likes'])) 
-
-	time_likes = pd.Series(data=df['likes'].values,index=df['date'])
-	time_likes.plot(figsize=(16,4),label="likes",legend=True)
-	time_retweet = pd.Series(data=df['retweets'].values,index=df['date'])
-	time_retweet.plot(figsize=(16,4),label="retweets",legend=True)
-	plt.show()
-	
+	df['sentiment'] = np.array([tweet_analyzer.get_sentiment(tweet) for tweet in df['Tweets']])
+	print(df)
+	print("Average sentiment score:",np.mean(df['sentiment']))	
